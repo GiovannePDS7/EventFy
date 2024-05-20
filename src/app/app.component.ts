18ac185903f8d6907a6acc4649ae68aabc7e19cb
Etec Prof. Horácio Augusto da Silveira <giovanne3282@gmail.com>
2024-05-20 11:30:18 -0300

    subindo arquivos base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrincipalComponent } from "./principal/principal.component";
+import { QuemSomosComponent } from "./quem-somos/quem-somos.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, PrincipalComponent, QuemSomosComponent]
 })
 export class AppComponent {
   title = 'EventFy';
