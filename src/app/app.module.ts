e3ce89918c6fb02ed6108c1b517e4deec191d121
Giovanne Pagano <giovanne3282@gmail.com>
2024-05-28 17:08:56 -0300

    desenvolvendo tela principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PrincipalComponent } from './principal/principal.component';
 
-import {MatCardModule} from '@angular/material/card';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { QuemSomosComponent } from './quem-somos/quem-somos.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { IntegrantesComponent } from './integrantes/integrantes.component';
 import { FooterComponent } from './footer/footer.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { HeaderComponent } from './header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -21,9 +26,14 @@ import { FooterComponent } from './footer/footer.component';
     QuemSomosComponent,
     EventosComponent,
     IntegrantesComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
+    FlexLayoutModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatGridListModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
