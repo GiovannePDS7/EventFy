ce0f907550d863e6e108ec7531716bb519819899
Giovanne Pagano <giovanne3282@gmail.com>
2024-06-10 19:18:16 -0300

    adicionando responsividade mobile

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,19 +6,5 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  NavHeader = document.getElementById('NavHeader');
-
-  isHeaderScrolled: boolean = false;
-
-  @HostListener('window:scroll', ['$event'])
-  onWindowScroll() {
-    this.isHeaderScrolled = window.scrollY > 0;
-    if(this.NavHeader){
-      if(window.scrollY == 0){
-        this.NavHeader.classList.add('text-top');
-      }else{
-        this.NavHeader.classList.add('text-scrolled');
-      }
-    }
-  }
+  
 }
