a740eef6a68dcb5c109c85a1b6dedbaecb4f006c
Giovanne Pagano <giovanne3282@gmail.com>
2024-06-21 22:42:18 -0300

    parte 1 navegação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
+import { HeaderComponent } from './header/header.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
+import { FooterComponent } from './footer/footer.component';
+import { IntegrantesComponent } from './integrantes/integrantes.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'principal', component: PrincipalComponent },
+  { path: 'quemsomos', component: QuemSomosComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'integrantes', component: IntegrantesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/quem-somos/quem-somos.component.ts
+++ src/app/quem-somos/quem-somos.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-quem-somos',
   templateUrl: './quem-somos.component.html',
   styleUrls: ['./quem-somos.component.css']
 })
-export class QuemSomosComponent {
+export class QuemSomosComponent implements OnInit {
+  constructor(private route: ActivatedRoute) { }
 
+  ngOnInit(): void {
+    this.route.fragment.subscribe(fragment => {
+      if (fragment) {
+        this.scrollToElement(fragment);
+      }
+    });
+  }
+
+  private scrollToElement(elementId: string): void {
+    const element = document.getElementById(elementId);
+    if (element) {
+      const yOffset = -12 * window.innerHeight / 100;
+      const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+      window.scrollTo({ top: y, behavior: 'smooth' });
+    }
+  }
 }
