eb736719cce5632e99166ef223cfeb98de9ecd5e
Giovanne Pagano <giovanne3282@gmail.com>
2024-06-22 15:45:06 -0300

    mudança integrantes desktop / adicionando imagens profissionais(incompleto))

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,54 +7,54 @@ import { Subscription } from 'rxjs';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent /*implements OnInit, OnDestroy*/ {
-  // private routerSubscription!: Subscription;
+export class FooterComponent implements OnInit, OnDestroy {
+  private routerSubscription!: Subscription;
 
-  // constructor(private router: Router) {}
+  constructor(private router: Router) {}
 
-  // ngOnInit(): void {
-  //   this.routerSubscription = this.router.events.subscribe(event => {
-  //     if (event instanceof NavigationEnd) {
-  //       const fragment = this.router.url.split('#')[1];
-  //       if (fragment) {
-  //         this.scrollToElement(fragment);
-  //       }
-  //     }
-  //   });
-  // }
+  ngOnInit(): void {
+    this.routerSubscription = this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const fragment = this.router.url.split('#')[1];
+        if (fragment) {
+          this.scrollToElement(fragment);
+        }
+      }
+    });
+  }
 
-  // ngOnDestroy(): void {
-  //   if (this.routerSubscription) {
-  //     this.routerSubscription.unsubscribe();
-  //   }
-  // }
+  ngOnDestroy(): void {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
 
-  // scrollToQuemSomos(): void {
-  //   this.navigateToSection('quem-somos', '/quemsomos');
-  // }
+  scrollToQuemSomos(): void {
+    this.navigateToSection('quem-somos', '/quemsomos');
+  }
 
-  // scrollToIntegrantes(): void {
-  //   this.navigateToSection('integrantes', '/integrantes');
-  // }
+  scrollToIntegrantes(): void {
+    this.navigateToSection('integrantes', '/integrantes');
+  }
 
-  // private navigateToSection(fragment: string, route: string): void {
-  //   const url = this.router.url.split('#')[0];
-  //   const newUrl = `${url}#${fragment}`;
+  private navigateToSection(fragment: string, route: string): void {
+    const url = this.router.url.split('#')[0];
+    const newUrl = `${url}#${fragment}`;
 
-  //   if (this.router.url === newUrl) {
-  //     this.scrollToElement(fragment);
-  //   } else {
-  //     this.router.navigate([route], { fragment });
-  //   }
-  // }
+    if (this.router.url === newUrl) {
+      this.scrollToElement(fragment);
+    } else {
+      this.router.navigate([route], { fragment });
+    }
+  }
 
-  // private scrollToElement(elementId: string): void {
-  //   const element = document.getElementById(elementId);
-  //   if (element) {
-  //     const yOffset = -12 * window.innerHeight / 100;
-  //     const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
-  //     window.scrollTo({ top: y, behavior: 'smooth' });
-  //   }
-  // }
+  private scrollToElement(elementId: string): void {
+    const element = document.getElementById(elementId);
+    if (element) {
+      const yOffset = -12 * window.innerHeight / 100;
+      const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+      window.scrollTo({ top: y, behavior: 'smooth' });
+    }
+  }
   
 }
