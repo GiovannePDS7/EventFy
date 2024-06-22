4817c861da963d1f615faa837628b6a3c1709634
Giovanne Pagano <giovanne3282@gmail.com>
2024-06-21 23:22:27 -0300

    ajustando cores e responsividade

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  constructor(private router: Router, private location: Location) {}
 
+  
+  scrollToQuemSomos(): void {
+    const url = this.router.url.split('#')[0];
+    const newUrl = url + '#quem-somos';
+
+    if (this.router.url === newUrl) {
+      this.scrollToElement('quem-somos');
+    } else {
+      this.router.navigate(['/quemsomos'], { fragment: 'quem-somos' });
+    }
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.scrollToElement('quem-somos');
+      }
+    });
+  }
+
+  scrollToIntegrantes(): void {
+    const url = this.router.url.split('#')[0]; 
+    const newUrl = url + '#';
+
+    if (this.router.url === newUrl) {
+      this.scrollToElement('integrantes');
+    } else {
+      this.router.navigate(['/integrantes'], { fragment: 'integrantes' });
+    }
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.scrollToElement('integrantes');
+      }
+    });
+  }
+
+  private scrollToElement(elementId: string): void {
+    const element = document.getElementById(elementId);
+    if (element) {
+      const yOffset = -80;
+      const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+    window.scrollTo({ top: y, behavior: 'smooth' });
+    }
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { Location } from '@angular/common';
 
